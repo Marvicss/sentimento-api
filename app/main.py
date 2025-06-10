9b7d099f3022f9e708537b5d396473b25b7719f9
Marvicss <marconivictor15@gmail.com>
2025-06-09 23:46:18 -0300

    ajuste no cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model import prever_sentimento
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Você pode trocar "*" por uma lista de domínios específicos ex: ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class EntradaTexto(BaseModel):
     frase: str
